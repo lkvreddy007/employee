6b7278cdd9b3ede910eed44098fda8d7527409e6
lkvreddy007 <lkvreddy007@gmail.com>
2020-09-22 09:03:19 +0530

    addingtobranch

---
+++ src/basicjava/empcheck.java
@@ -0,0 +1,15 @@
+package basicjava;
+
+public class empcheck {
+	public static void main(String[] args) {
+		int isfulltime=1;
+		double empcheck= Math.floor(Math.random()*10)%2; 
+		if(empcheck==isfulltime) {
+			System.out.println("Employee present");
+		}
+		else {
+			System.out.println("Employee absent");
+		}
+	}
+
+}
